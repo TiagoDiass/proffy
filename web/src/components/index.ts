a7f152a1e9a1b1497ff4dda6c4250dedffbf4793
TiagoDiass <tiago.costadiasss@gmail.com>
2021-03-04 22:36:42 -0300

    deleted web front-end folder

--- web/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as PageHeader } from './PageHeader/PageHeader';
-export { default as TeacherItem } from './TeacherItem/TeacherItem';
-export { default as Input } from './Input/Input';
-export { default as Select } from './Select/Select';
-export { default as TextArea } from './TextArea/TextArea';
--- web/src/pages/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as Landing} from './Landing/Landing'
-export {default as TeacherList} from './TeacherList/TeacherList'
-export {default as TeacherForm} from './TeacherForm/TeacherForm'
